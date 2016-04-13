372451088d77ee216f58c96720ec658dabdd255a
jedixo <jake.dixon@my.jcu.edu.au>
2016-04-13 11:32:29 +1000

    added basic 2p play

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var io = require('socket.io')(http);
 
 //variables
 var port = 3000;
+var clients = 0;
+var p1 = 0;
+var p2 = 0;
 
 //forwards html to clients
 app.get('/', function(req, res){
@@ -12,11 +15,24 @@ app.get('/', function(req, res){
 
 // connection listener
 io.on('connection', function(socket){
+    clients++;
+    io.emit('client number',clients);
+    
     
     socket.on('player mouse position', function(msg) {
         console.log("Player:" + msg.player + ": PosY: " + msg.position)
-        io.emit('latency test', msg.position);
+        if (msg.player == 1) {
+            p1 = msg.position;
+        } else if (msg.player == 2) {
+            p2 = msg.position;
+        }
+        
+        io.emit('paddles position',{paddle1: p1, paddle2: p2});
     });
+    
+    socket.on('ball pos', function(msg) {
+        io.emit('p2 ball pos', msg);
+    })
 });
 
 
