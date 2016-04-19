15e05ad01c345c0c335ded2504f8016867780fa8
jedixo <jake.dixon@my.jcu.edu.au>
2016-04-19 21:34:01 +1000

    some updates to the class

--- Ball.js
+++ Ball.js
@@ -3,15 +3,15 @@ function Ball () {}
 Ball.prototype.xy = {x:0, y:0};
 Ball.prototype.direction = 0;
 Ball.prototype.move = function () {
-
+  //function for moving the ball
 };
 Ball.prototype.redirect = function () {
-
+  //function for redirecting the ball
 };
 Ball.prototype.draw = function () {
-
+  //function for drawing the ball
 };
 
-//test ball
+//test ball initalisation
 var ball = new Ball();
 console.log(ball.xy);
