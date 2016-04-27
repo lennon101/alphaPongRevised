96aec95767a2b01520fe8f7f54b887d880a16aa1
Dane Lennon <dane.lennon101@gmail.com>
2016-04-27 11:35:42 +1000

    add document comments to paddle class and amend hitTest params to ensure paddle does not rely on knowing about ball class

--- Paddle.js
+++ Paddle.js
@@ -1,12 +1,21 @@
 //paddle class
+/**
+ * A Paddle Class that controls a paddle and if anything hits it
+ * 
+ * @param x the x position of the paddle to define player 1 or player 2 
+ */
 function Paddle(x) {
     this.player = 1;
-    this.position = {x: (x)?x:10, y: 25};
+    this.position = {x: (x)?x:10, y: 25}; //if x is underfined it defaults to 10
     this.dimensions = {length: 100, width: 10};
     this.colour = '#00FF00';
     this.strokeLineWidth = 5;
 
-    //@param ctx canvas context for drawing paddle
+    /**
+     * draws the paddle
+     * 
+     * @param ctx canvas context for drawing paddle
+     */
     this.draw = function(ctx) {
         ctx.strokeStyle = this.colour;
         ctx.fillStyle = this.colour;
@@ -15,25 +24,45 @@ function Paddle(x) {
         ctx.fillRect(this.position.x, this.position.y, this.dimensions.width, this.dimensions.length);
     };
 
-    //moves the paddle to the given Y cordinate
+    /**
+     * moves the paddle to the given Y cordinate
+     * 
+     * @param y the y postion of the paddle
+     */
     this.move = function(y) {
         this.position.y += y;
     };
 
-    //sets the colour of the paddle
+    /**
+     * sets the colour of the paddle
+     * 
+     * @param colour the colour to set the paddle
+     */
     this.setColour = function(colour){
         this.colour = colour;
     };
     
+    /**
+     * get the y position of the paddle
+     * 
+     * @returns the y position of the paddle
+     */
     this.getPosition = function(){
         return this.position.y;
     }
     
-    this.hitTest = function(ball){
-        if(ball.position.x <= (this.position.x + this.dimensions.width) && (ball.position.y >= this.position.y && ball.position.y <= (this.position.y + this.dimensions.length))){
-//        if (ball.position.y < this.position.y + 1/3*this.dimensions.length){
+    /**
+     * test if paddle has been hit by an object
+     * 
+     * @param x the postion x of the object
+     * @param y the position y of the object
+     * @returns true or false for hit by object
+     */
+    this.hitTest = function(x,y){
+        if(x <= (this.position.x + this.dimensions.width) && (y >= this.position.y && y <= (this.position.y + this.dimensions.length))){
+//        if (y < this.position.y + 1/3*this.dimensions.length){
 //            console.log("upper third hit")
-//        } else if ((ball.position.y > this.position.y + 1/3*this.dimensions.length) &&(ball.position.y < this.position.y + 2/3*this.dimensions.length)){
+//        } else if ((y > this.position.y + 1/3*this.dimensions.length) &&(y < this.position.y + 2/3*this.dimensions.length)){
 //            console.log("middle third hit")
 //        } else{
 //            console.log("lower third hit")
