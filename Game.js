1019e4f9286e64e665cd11a349f18056e617a08e
jedixo <jake.dixon@my.jcu.edu.au>
2016-04-27 20:19:07 +1000

    made Game.js
    - moved all the js from text.html into game.js
    - changed test.html to the new index.html
    - renamed old index.html for reference

--- Game.js
+++ Game.js
@@ -1,24 +1,30 @@
-window.onload = function(){
+   
+        var balls = [];
+        
+        window.onload = function(){
             var canvas = document.getElementById("pongCanvas");
+        
             if (canvas && canvas.getContext) {
                 var ctx = canvas.getContext("2d");
             }
-            player = 1;
-            paddles = {p1: new Paddle(), p2: new Paddle()};
+            var paddles = {p1: new Paddle(), p2: new Paddle()};
             var paddle = new Paddle();
-            var ball = new Ball();
-            ball.position = {x:canvas.width / 2, y: canvas.height / 2};
+            //var ball = new Ball();
             
-            
+            for (var i = 0;i<5;++i){
+                balls.push(new Ball());
+                balls[i].position = {x:canvas.width / 2, y: canvas.height / 2};
+            }
+
             //controlls paddle position
             canvas.addEventListener('mousemove', function(evt) {
                 var rect = canvas.getBoundingClientRect();
                 paddle.position.y = Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height);
                 //makes sure paddle doesn't go off screen
-                if (paddle.position.y < paddle.dimensions.length / 10) {
-                    paddle.position.y = paddle.dimensions.length / 10;
-                } else if (paddle.position.y > canvas.height - paddle.dimensions.length - paddle.dimensions.length / 10) {
-                    paddle.position.y = canvas.height - paddle.dimensions.length - paddle.dimensions.length / 10;
+                if (paddle.position.y < paddle.dimensions.length / 100) {
+                    paddle.position.y = paddle.dimensions.length / 100;
+                } else if (paddle.position.y > canvas.height - paddle.dimensions.length - paddle.dimensions.length / 100) {
+                    paddle.position.y = canvas.height - paddle.dimensions.length - paddle.dimensions.length / 100;
                 }
             });
             
@@ -27,13 +33,23 @@ window.onload = function(){
                 ctx.fillStyle = "#000";
                 ctx.fillRect(0, 0, canvas.width, canvas.height);
                 paddle.draw(ctx);
-                ball.draw(ctx);
-                //ball.move();
-                if (ball.position.x >= canvas.width || ball.position.x <= 0) {
-                    ball.bounce("x");
-                } else if (ball.position.y >= canvas.height || ball.position.y <= 0) {
-                    ball.bounce("y");
+               // if(paddle.hitTest(ball) || paddle2.hitTest(ball)){
+                
+                for (var i = 0;i<5;++i){
+                    if (paddle.hitTest(balls[i].position.x, balls[i].position.y)) {
+                        balls[i].bounceX();
+                    }
+                    balls[i].draw(ctx);
+                    
+                    if (balls[i].position.x >= canvas.width || balls[i].position.x <= 0) {
+                        balls[i].bounceX();
+                    } else if (balls[i].position.y >= canvas.height || balls[i].position.y <= 0) {
+                        balls[i].bounceY();
+                    }
                 }
+                
+                //ball.move();
+
                 requestAnimationFrame(game);
             }
             game();
