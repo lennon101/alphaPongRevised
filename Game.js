dff6ebe1eef29be99c3b5464f22f34816c59561f
jake Dixon <jake.dixon@my.jcu.edu.au>
2016-05-25 01:02:43 +1000

    mb thorugh socket

--- Game.js
+++ Game.js
@@ -52,7 +52,10 @@ socket.on('getPlayerNumber', function (msg) {
  */
 socket.on("ball", function (ball) {
     if (player > 1) {
+        balls = [];
         for (var i = 0; i < ball.length; i++) {
+            
+            balls.push(new Ball());
             balls[i].position = ball[i].position;
             balls[i].velocity = ball[i].velocity;
             balls[i].trailColour = ball[i].trailColour;
